ca7d20ea429633bf29fb39e729221facf8acef0f
Victor Augusto <victoraweb@gmail.com>
2018-09-09 17:13:39 -0300

    Initial structure

---
+++ src/main/kotlin/com/augustovictor/cookinglab/CookingLabApplication.kt
@@ -0,0 +1,11 @@
+package com.augustovictor.cookinglab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CookingLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<CookingLabApplication>(*args)
+}
---
+++ src/test/kotlin/com/augustovictor/cookinglab/CookingLabApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.augustovictor.cookinglab
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CookingLabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
